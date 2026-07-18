83c3455fb233f029bd0c0af8dd9bbcf1f7c00606
Insee <insee134@gmail.com>
2026-07-18 12:33:12 +0700

    TIMUS

--- TIMUS/1012/sol.cpp
+++ TIMUS/1012/sol.cpp
@@ -2,11 +2,6 @@
  
 using namespace std;
  
-#ifdef LOCAL
-#include "debug.h"
-#else
-#define dbg(...) 42
-#endif
 #define int long long
 #define sz(x) (int)(x).size()
 const int di[4] = {0, 0, -1, 1};
@@ -16,48 +11,46 @@ const int dj[4] = {-1, 1, 0, 0};
  
 void solve() {
     int N, K; cin >> N >> K;
-    vector<string> dp(N + 1, "");
-    dp[0] = to_string(K - 1);
-    dp[1] = to_string((K - 1) * K);
-    auto add = [&](string a, string b) {
-	a = '0' + a;
-	b = string(sz(a) - sz(b), '0') + b;
-	string c = string(sz(a), '0');
-    	for (int i = sz(a) - 1; i > 0 ;i--) {
-		int rA = a[i] - '0';
-		int rB = b[i] - '0';
-		int rC = c[i] - '0';
-		c[i] = (char)((rC + rA + rB) % 10 + '0');
-		c[i - 1] = (char)(c[i - 1] + (rC + rA + rB) / 10);
+    vector<vector<int> > dp(N + 1);
+    dp[0] = {K - 1};
+    dp[1] = {(K - 1) * K};
+    const int base = 1000*1000*1000;
+    auto add = [](vector<int> a, vector<int> b) {
+	int carry = 0;
+	for (size_t i = 0; i < max(a.size(), b.size()) || carry; i++) {
+	    if (i == a.size())
+		a.push_back(0);
+	    a[i] += carry + (i < b.size() ? b[i] : 0);
+	    carry = a[i] >= base;
+	    if (carry) a[i] -= base;
 	}
-	int tmp = 0;
-	while (c[tmp] == '0') {
-		tmp++;
-	}
-	c.erase(0, tmp);
-	return c;
+	return a;
     };
-    auto mul = [&](string a, int b) {
-	a = '0' + a;
-	string c = string(sz(a), '0');
-	for (int i = sz(a) - 1; i > 0; i--) {
-		int rA = a[i] - '0';
-		int rC = c[i] - '0';
-		c[i] = (char)((rC + rA * b) % 10 + '0');
-		c[i - 1] = (char)(c[i - 1] + (rC + rA * b) / 10);
+    auto mul = [](vector<int> a, int b) {
+	int carry = 0;
+	for (size_t i = 0; i < a.size() || carry; i++) {
+	    if (i == a.size())
+		a.push_back(0);
+	    int cur = carry + a[i] * b;
+	    carry = (int)(cur / base);
+	    a[i] = cur - carry * base;
 	}
-	int tmp = 0;
-	while(c[tmp] == '0') {
-		tmp++;
-	}
-	c.erase(0, tmp);
-	return c;
+	while (sz(a) > 1 && a.back() == 0) 
+	    a.pop_back();
+	return a;
     };
     for (int i = 2; i < N; i++) {
-	string res =add(dp[i - 1], dp[i - 2]);
+	vector<int> res =add(dp[i - 1], dp[i - 2]);
 	dp[i] = mul(res, K - 1);
     }
-    cout << dp[N - 1] << '\n';
+    vector<int> A = dp[N - 1];
+    string result = to_string(A.empty() ? 0: A.back());
+    for (int i = sz(A) - 2; i >= 0; i--) {
+	string chuck = to_string(A[i]);
+	chuck = string(9 - sz(chuck), '0') + chuck;
+	result += chuck;
+    }
+    cout << result << '\n';
 }
  
 int32_t main() {
