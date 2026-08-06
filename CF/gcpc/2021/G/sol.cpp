7e3e07a0f9337f9bb78c36cd36ac0fde57b0b5c8
Insee <insee134@gmail.com>
2026-08-06 16:12:55 +0700

    CF

--- CF/gcpc/2021/G/sol.cpp
+++ CF/gcpc/2021/G/sol.cpp
@@ -9,22 +9,26 @@ using namespace std;
 void sol() {
 	int H, W; cin >> H >> W;
 	vector<vector<int> > G(W);
+	vector<int> lasts(W);
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
 			char x; cin >> x;
-			if (x == 'C') G[j].push_back(i);
+			if (x == 'C') {
+			    G[j].push_back(i);
+			    lasts[j]++;
+			}
 		}
 	}
 	int cnt = 0;
 	for (int j = 0; j < W; j++) {
-		if (!G[j].empty()) {
-		    int last = G[j].back();
+		if (!G[j].empty() && lasts[j] != 0) {
+		    int at = G[j][lasts[j] - 1];
 		    for (int k = j + 1; k < W; k++) {
-			auto tmp = lower_bound(G[k].begin(), G[k].end(), last);
-			if (tmp == G[k].end()) continue;
+			int tmp = lower_bound(G[k].begin(), G[k].begin() + lasts[k], at) - G[k].begin();
+			if (tmp >= lasts[k]) continue;
 			else {
-			    last = G[k].back();
-			    G[k].erase(tmp, G[k].end());
+			    at = G[k][lasts[k] - 1];
+			    lasts[k] = tmp;
 			}
 		    }
 		    cnt++;
