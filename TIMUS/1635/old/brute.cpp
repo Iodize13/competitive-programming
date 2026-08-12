a21f82b7caabcb2fe08eb69b19b5aac15e0e09da
Insee <insee134@gmail.com>
2026-08-12 17:54:16 +0700

    TIMUS

---
+++ TIMUS/1635/old/brute.cpp
@@ -0,0 +1,64 @@
+// #pragma GCC optimize("O3,unroll-loops")
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...) 42
+#endif
+#define send {ios_base::sync_with_stdio(false);}
+#define help {cin.tie(NULL);}
+#define fi first
+#define se second
+#define int long long
+#define ll long long
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+void solve();
+const int di[4] = {0, 0, -1, 1};
+const int dj[4] = {-1, 1, 0, 0};
+const int inf = (int)2e18 + 5;
+
+int32_t main() {
+    send help
+
+    solve();
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = sz(s);
+    vector<string> a;
+    vi dp(n+1, inf), p(n+1);
+    dp[0] = 0;
+    auto is_palindrome = [&](string x) {
+        return equal(all(x) - sz(x) / 2, x.rbegin());
+    };
+
+    rep(i,1,n+1) {
+        rep(j,0,i) {
+            if (s[i-1] == s[j] && is_palindrome(s.substr(j, i - j))) {
+                if (1 + dp[j] < dp[i]) {
+                    dp[i] = 1 + dp[j];
+                    p[i] = j;
+                }
+            }
+        }
+    }
+
+    cout << dp[n] << '\n';
+    int cur = n;
+    while (cur) {
+        a.push_back(s.substr(p[cur], cur - p[cur]));
+        cur = p[cur];
+    }
+    reverse(all(a));
+    for (auto &x: a) cout << x << ' ';
+    cout << '\n';
+}
--- TIMUS/1635/sol.cpp
+++ TIMUS/1635/sol.cpp
@@ -1,115 +1,53 @@
-// #pragma GCC optimize("O3,unroll-loops")
 #include "bits/stdc++.h"
 
 using namespace std;
 
-#ifdef LOCAL
-#include "debug.h"
-#else
-#define dbg(...) 42
-#endif
 #define send {ios_base::sync_with_stdio(false);}
 #define help {cin.tie(NULL);}
-#define fi first
-#define se second
 #define int long long
-#define ll long long
 #define sz(x) (int)(x).size()
-#define all(x) (x).begin(), (x).end()
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-void solve();
-const int di[4] = {0, 0, -1, 1};
-const int dj[4] = {-1, 1, 0, 0};
-const int inf = (int)2e18 + 5;
 
-int32_t main() {
-    send help
-
-    solve();
-}
 
 void solve() {
-    string s;
-    cin >> s;
-    int n = sz(s);
-    vector<string> a;
-    vi best(n);
-    auto is_palindrome = [&](string x) {
-        return equal(all(x) - sz(x) / 2, x.rbegin());
-    };
+	const int inf = (int)2e18 + 5;
+	string S; cin >> S;
+	int N = sz(S);
+	vector<vector<bool> > ok(N, vector<bool>(N, false));
+	vector<int> dp(N + 1, inf), p(N + 1, 0);
+	vector<string> ans;
+	dp[0] = 0;
+	for (int i = 0; i < N; i++) ok[i][i] = true;
+	for (int len = 1; len <= N; len++) {
+		for (int i = 0, j = len - 1; j < N; i++, j++) {
+			if (S[i] == S[j])
+				ok[i][j] = (i + 1 <= j - 1) ? ok[i + 1][j - 1] : true;
+		}
+	}
+	// cerr << "enter 5" << endl;
+	for (int j = 1; j <= N; j++) {
+		for (int i = 0; i < j; i++) {
+			if (dp[i] + 1 < dp[j] && ok[i][j - 1]) {
+				// cerr << "enter 5.5" << endl;
+				dp[j] = dp[i] + 1;
+				p[j] = i;
+			}
+		}
+	}
+	// cerr << "enter 6" << endl;
 
-    int cur = 0;
-    string first = "";
-    int ind = 0;
-    rep(i,0,n) {
-        for (int j = n - 1; j >= i; j--) {
-            if (s[i] == s[j] && is_palindrome(s.substr(i, j - i + 1))) {
-                best[i] = j - i + 1;
-                if (best[i] > cur) {
-                    cur = best[i];
-                    first = s.substr(i, best[i]);
-                    ind = i;
-                }
-                break;
-            }
-        }
-    }
-
-    auto dfs = [&](auto &&self, int l, int r) {
-        if (r < l) return;
-        if (r == l) {
-            a.push_back(s.substr(l, 1));
-            return;
-        }
-        int mx = 0;
-        int ind0 = l;
-        string t = "";
-        rep(i,l,r+1) {
-            // cerr << "i: " << i << '\n';
-            if (best[i] + i - 1 > r) {
-                best[i] = 1;
-                for (int j = r; j >= i; j--) {
-                    if (s[i] == s[j] && is_palindrome(s.substr(i, j - i + 1))) {
-                        best[i] = j - i + 1;
-                        break;
-                    }
-                }
-            }
-            if (best[i] > mx) {
-                mx = best[i];
-                t = s.substr(i, best[i]);
-                ind0 = i;
-            }
-        }
-
-        //cerr << "l: " << l << ", ind0: " << ind0 << ", r: " << r << '\n';
-        self(self, l, ind0 - 1);
-        //cerr << "dbg:\n";
-        //for (auto &x: a) cerr <<x << ' ';
-        //cerr << '\n';
-        dbg(best[ind0]);
-        //cerr << "l: " << l << ", ind0: " << ind0 << ", r: " << r << '\n';
-        dbg(t);
-        a.push_back(t);
-        self(self, ind0 + best[ind0], r);
-    };
-    dfs(dfs, 0, ind - 1);
-    //cerr << "dbg 0:\n";
-    //for (auto &x: a) cerr <<x << ' ';
-    //cerr << '\n';
-    a.push_back(first);
-    //cerr << "dbg 1:\n";
-    //for (auto &x: a) cerr <<x << ' ';
-    //cerr << '\n';
-    dbg(cur);
-    dbg(ind);
-    dbg(best[ind]);
-    dfs(dfs, ind + best[ind], n - 1);
-
-    cout << sz(a) << '\n';
-    for (auto &x: a) cout << x << ' ';
-    cout << '\n';
+	int cur = N;
+	while (cur) {
+		ans.push_back(S.substr(p[cur], cur - p[cur]));
+		cur = p[cur];
+	}
+	// cerr << "enter 7" << endl;
+	reverse(ans.begin(), ans.end());
+	cout << dp[N] << '\n';
+	for (auto x: ans) cout << x << ' ';
 }
 
+int32_t main() {
+	send help
+
+		solve();
+}
