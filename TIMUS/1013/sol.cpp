340cf687526146668736429be57846892d037e27
Insee <insee134@gmail.com>
2026-08-13 21:51:00 +0700

    TIMUS

--- TIMUS/1013/sol.cpp
+++ TIMUS/1013/sol.cpp
@@ -6,6 +6,18 @@ using namespace std;
 #define sz(x) (int)(x).size()
  
 int M;
+
+int mul (int a, int b) {
+	int ans = 0;
+	while (b) {
+		if (b & 1) ans = (ans + a) % M;
+		b >>= 1;
+		a <<= 1;
+		a %= M;
+	}
+	return ans;
+}
+
 struct matrix {
 	int mat[2][2];
 	matrix friend operator *(const matrix &a, const matrix &b) {
@@ -14,7 +26,7 @@ struct matrix {
 			for (int j = 0; j < 2; j++) {
 				c.mat[i][j] = 0;
 				for (int k = 0; k < 2; k++) {
-					c.mat[i][j] = (c.mat[i][j] + (__int128_t)a.mat[i][k] * b.mat[k][j] % M) % M;
+					c.mat[i][j] = (c.mat[i][j] + mul(a.mat[i][k], b.mat[k][j]) % M) % M;
 				}
 			}
 		}
@@ -22,21 +34,27 @@ struct matrix {
 	}
 };
 
+matrix matpow(matrix base, int n) {
+	matrix ans{ {
+		{1, 0},
+		{0, 1}
+	} };
+	while (n) {
+		if (n & 1)
+			ans = ans*base;
+		base = base*base;
+		n >>= 1;
+	}
+	return ans;
+}
+
 void solve() {
 	int N, K; cin >> N >> K >> M;
-	matrix A{{
-			 { (K - 1) % M	, (K - 1) % M	},
-			 { 1			, 0				}
-			}};
-	matrix ans{{
-				 {1, 0},
-				 {0, 1}
-				 }};
-	matrix j = A;
-	for (int i = N; i > 0; i>>=1, j = j * j) {
-		if (i & 1) ans = ans * j;
-	}
-	cout << ans.mat[0][0] << '\n';
+	matrix A{ {
+		{ (K - 1) % M	, (K - 1) % M	},
+		{ 1				, 0				}
+	} };
+	cout << matpow(A, N).mat[0][0] << '\n';
 }
  
 int32_t main() {
