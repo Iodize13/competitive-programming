5194fd1b911c8855a1fa3cf26c07465bb479158f
Insee <insee134@gmail.com>
2026-08-31 20:07:30 +0700

    TIMUS

--- TIMUS/1208/sol.cpp
+++ TIMUS/1208/sol.cpp
@@ -1,67 +1,73 @@
-// #pragma GCC optimize("O3,unroll-loops")
 #include "bits/stdc++.h"
 
 using namespace std;
 
-#ifdef LOCAL
-#include "debug.h"
-#else
-#define dbg(...) 42
-#endif
 #define send {ios_base::sync_with_stdio(false);}
 #define help {cin.tie(NULL);}
-#define f first
-#define s second
 #define int long long
-#define ll long long
 #define sz(x) (int)(x).size()
-#define all(x) (x).begin(), (x).end()
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-const int di[4] = {0, 0, -1, 1};
-const int dj[4] = {-1, 1, 0, 0};
 
 
-
-const bool TC = 0;
-const int K = 18;
-string a[K + 1][3];
 void solve() {
-    int n;
-    cin >> n;
-    rep(i,0,n) {
-        rep(j,0,3) {
-            cin >> a[i][j];
-        }
-    }
+	int K; cin >> K;
+	vector<vector<string> > A(K);
+	vector<string> names;
+	for (int i = 0; i < K; i++) {
+		for (int j = 0; j < 3; j++) {
+			string x; cin >> x;
+			A[i].push_back(x);
+			names.push_back(x);
+		}
+	}
+	sort(names.begin(), names.end());
+	names.resize(unique(names.begin(), names.end()) - names.begin());
+	stack<array<int, 3> > st;
+	vector<string> cur;
+	st.push({0, 0, 0});
+	vector<bool> vis(K);
+	int ans = 1;
+	while (!st.empty()) {
+		auto &top = st.top();
+		// cerr << top[0] << ' ' << top[1] << ' ' << top[2] << endl;
+		if (top[0] == K) {
+			ans = max(ans, top[2]);
+			st.pop();
+			continue;
+		}
+		if (top[1] == 0) {
+			bool ok = true;
+			top[1]++;
+			for (int i = 0; i < 3; i++) {
+				if (find(cur.begin(), cur.end(), A[top[0]][i]) != cur.end()) {
+					ok = false;
+					break;
+				}
+			}
+			if (!ok) {
+				continue;
+			}
+			for (int i = 0; i < 3; i++) {
+				cur.push_back(A[top[0]][i]);
+			}
+			st.push({top[0] + 1, 0, top[2] + 1});
+			vis[top[0]] = true;
+		} else if (top[1] == 1) {
+			// have problem when never push
+			if (vis[top[0]]) 
+				for (int i = 0; i < 3; i++) cur.pop_back();
 
-    auto f = [&](auto &&self, int x, bool take, map<string, bool> dp) {
-        if (x == 0) return 0ll;
-        if (take) {
-            rep(j,0,3) {
-                dp[a[x][j]] = true;
-            }
-        }
-        int ret = self(self, x - 1, 0, dp);
-        bool good = true;
-        rep(i,0,3) {
-            good &= !dp[a[x-1][i]];
-        }
-        if (good) ret = max(ret, self(self, x - 1, 1, dp) + 1);
-        return ret;
-    };
-    
-    map<string, bool> dp0;
-    int f0 = f(f, n-1,0, dp0);
-    int f1 = f(f, n-1,1, dp0) + 1;
-    cout << max(f0, f1) << '\n';
+			vis[top[0]] = false;
+			st.push({top[0] + 1, 0, top[2]});
+			top[1]++;
+		} else {
+			st.pop();
+		}
+	}
+	cout << ans << '\n';
 }
 
 int32_t main() {
     send help
 
-    int tt = 1;
-    if (TC) cin >> tt;
-    while (tt--) solve();
+    solve();
 }
