75f75029139b577d1e9ffc60afd8b0ca0cfa5c19
Insee <insee134@gmail.com>
2026-09-02 19:37:59 +0700

    TIMUS

--- TIMUS/1002/sol.cpp
+++ TIMUS/1002/sol.cpp
@@ -1,38 +1,89 @@
-// #pragma GCC optimize("O3,unroll-loops")
 #include "bits/stdc++.h"
 
 using namespace std;
 
-#ifdef LOCAL
-#include "debug.h"
-#else
-#define dbg(...) 42
-#endif
-#define send {ios_base::sync_with_stdio(false);}
-#define help {cin.tie(NULL);}
-#define fi first
-#define se second
+#define send                                                                   \
+{                                                                            \
+	ios_base::sync_with_stdio(false);                                          \
+}
+#define help                                                                   \
+{                                                                            \
+	cin.tie(NULL);                                                             \
+}
 #define int long long
-#define ll long long
 #define sz(x) (int)(x).size()
-#define all(x) (x).begin(), (x).end()
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-void solve();
-int case_num = 0;
-const int di[4] = {0, 0, -1, 1};
-const int dj[4] = {-1, 1, 0, 0};
+
+
+const int inf = (int) 1e9;
+void solve(string S) {
+	int N = sz(S);
+	int K;
+	cin >> K;
+	map<string, string> M;
+	vector<int> az('z' + 1);
+	az['i'] = az['j'] = '1';
+	az['a'] = az['b'] = az['c'] = '2';
+	az['d'] = az['e'] = az['f'] = '3';
+	az['g'] = az['h'] = '4';
+	az['k'] = az['l'] = '5';
+	az['m'] = az['n'] = '6';
+	az['p'] = az['r'] = az['s'] = '7';
+	az['t'] = az['u'] = az['v'] = '8';
+	az['w'] = az['x'] = az['y'] = '9';
+	az['o'] = az['q'] = az['z'] = '0';
+	for (int i = 0; i < K; i++) {
+		string T;
+		cin >> T;
+		string newS = "";
+		for (int j = 0; j < sz(T); j++) {
+			newS += az[(int)T[j]];
+		}
+		M[newS] = T;
+	}
+	vector<int> dp(N + 1, inf);
+	vector<string> path(N + 1);
+	dp[0] = 0;
+	path[0] = "-1";
+	for (int i = 1; i < N + 1; i++) {
+		for (int j = 0; j <= i; j++) {
+			string tmp = S.substr(j, i - j);
+			// cerr << tmp << '\n';
+			if (M.count(tmp) == 1) {
+				// cerr << "case: 5\n";
+				if (dp[j] + 1 < dp[i]) {
+					// cerr << "case: 6\n";
+					dp[i] = dp[j] + 1;
+					path[i] = M[tmp];
+				}
+			}
+		}
+	}
+	if (dp[N] != inf) {
+		int cur = N;
+		vector<string> ans;
+		while (cur != 0) {
+			ans.push_back(path[cur]);
+			cur -= sz(path[cur]);
+		}
+		for (int i = sz(ans) - 1; i >= 0; i--) cout << ans[i] << " \n"[i == 0];
+	} else {
+		cout << "No solution.\n";
+	}
+}
 
 int32_t main() {
-    send help
+	send help
 
-    solve();
+		string S;
+
+	while (cin >> S) {
+		if (S == "-1") {
+			return 0;
+		}
+
+		solve(S);
+	}
 }
 
-void solve() {
-    int n, a, b;
-    cin >> n >> a >> b;
-    cout << n * a * b * 2ll << '\n';
-}
-
+// wrong case 0000000 00 00 00 00 00 00 00
+// instead of 00 0000000000000000000000000
